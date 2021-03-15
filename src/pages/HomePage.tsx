22ff1cfa8d5164f1d482cd73601b5169753eed5d
Beatrice Guerra <betrix.pongo@gmail.com>
2021-03-16 00:46:05 +0100

    TRAK-20 add card to contain a track

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import {
+	Button,
+	Card,
+	CardContent,
+	CardHeader,
+	Container,
+	IconButton,
+	Typography,
+} from '@material-ui/core';
+import MoreVerticalIcon from '@material-ui/icons/MoreVert';
+import DirectionsWalkRoundedIcon from '@material-ui/icons/DirectionsWalkRounded';
 import { HeaderBar } from '../components/HeaderBar';
-import { Text } from '../components/Text';
 
-export const HomePage: React.FC = () => (
-	<>
-		<HeaderBar>
-			<Text type="h6">Track finder</Text>
-		</HeaderBar>
-		<Container>Main list here</Container>
-	</>
-);
+export const HomePage: React.FC = () => {
+	const cardAction = () => {
+		return (
+			<IconButton>
+				<MoreVerticalIcon />
+			</IconButton>
+		);
+	};
+
+	return (
+		<>
+			<HeaderBar>
+				<Typography variant="h6">Track finder</Typography>
+			</HeaderBar>
+			<Container>
+				<Card>
+					<CardHeader action={cardAction} title="Track 1" subheader="16 marzo 2021" />
+					<CardContent>
+						<Typography variant="body2">Track descripton preview</Typography>
+						<Button variant="contained" color="primary" startIcon={<DirectionsWalkRoundedIcon />}>
+							Distance
+						</Button>
+					</CardContent>
+				</Card>
+			</Container>
+		</>
+	);
+};
