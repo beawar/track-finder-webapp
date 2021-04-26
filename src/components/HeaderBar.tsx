e37774b2e45e3faf7146269aa53511ec2ed9aa39
Beatrice Guerra <betrix.pongo@gmail.com>
2021-04-26 23:46:55 +0200

    TRAK-18 add logo and set drawer width

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -8,17 +8,36 @@ import {
 	ListItem,
 	ListItemText,
 	Toolbar,
-	Typography,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import styled from '@emotion/styled';
 import { HideOnScroll } from './HideOnScroll';
 import { route } from '../routes';
+import LogoImg from '../assets/logo.png';
 
 interface HeaderBarProps {
 	createOption?: boolean;
 }
+
+const Logo = styled.img`
+	max-height: 70px;
+`;
+
+const CustomDrawer = styled(Drawer)`
+	& .MuiDrawer-paper {
+		${({ theme }) => theme.breakpoints.up('md')} {
+			width: 25%;
+		}
+		${({ theme }) => theme.breakpoints.down('md')} {
+			width: 40%;
+		}
+		${({ theme }) => theme.breakpoints.down('sm')} {
+			width: 100%;
+		}
+	}
+`;
 
 export const HeaderBar: React.FC<HeaderBarProps> = ({ children, createOption = true }) => {
 	const [drawerOpen, setDrawerOpen] = useState(false);
@@ -28,9 +47,12 @@ export const HeaderBar: React.FC<HeaderBarProps> = ({ children, createOption = t
 		setDrawerOpen((prevState) => !prevState);
 	}, []);
 
-	const addNewTrack = useCallback(() => {
-		history.push(route.edit);
-	}, [history]);
+	const goToPath = useCallback(
+		(path) => () => {
+			history.push(path);
+		},
+		[history]
+	);
 
 	return (
 		<>
@@ -41,31 +63,32 @@ export const HeaderBar: React.FC<HeaderBarProps> = ({ children, createOption = t
 							<MenuIcon />
 						</IconButton>
 						<NavLink to={route.home}>
-							<Typography variant="h6">Track finder</Typography>
+							<Logo src={LogoImg} alt="Track finder" />
 						</NavLink>
 						{children}
 						{createOption && (
-							<IconButton edge="end" onClick={addNewTrack}>
+							<IconButton edge="end" onClick={goToPath(route.edit)}>
 								<AddIcon fontSize="large" />
 							</IconButton>
 						)}
 					</Toolbar>
 				</AppBar>
 			</HideOnScroll>
-			<Drawer open={drawerOpen} anchor="left">
+			<CustomDrawer open={drawerOpen} anchor="left">
 				<Toolbar>
 					<IconButton onClick={toggleDrawer} edge="end">
 						<ChevronLeftIcon />
 					</IconButton>
 				</Toolbar>
 				<List>
-					{['Boh', 'Boh ma piu lungo'].map((text) => (
-						<ListItem button key={text}>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
+					<ListItem button onClick={goToPath(route.home)}>
+						<ListItemText primary="Home" />
+					</ListItem>
+					<ListItem button onClick={goToPath(route.edit)}>
+						<ListItemText primary="Create new track" />
+					</ListItem>
 				</List>
-			</Drawer>
+			</CustomDrawer>
 			{/* this toolbar is used to fill the space under the appbar,
 			so that the main content is not hidden */}
 			<Toolbar />
